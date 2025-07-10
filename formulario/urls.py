6542235a24e8b899b402fa57cb1981370c043964
Jesdavtor <jestorniak@gmail.com>
2025-07-10 13:47:49 -0400

    Hacer que /formulario/ muestre el formulario de contacto

--- formulario/urls.py
+++ formulario/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import ContactoCreateView, ContactoExitoView, ContactoListAPI
 
 urlpatterns = [
+    path('', ContactoCreateView.as_view(), name='formulario_home'),
     path('contacto/', ContactoCreateView.as_view(), name='contacto'),
     path('contacto/exito/', ContactoExitoView.as_view(), name='contacto_exito'),
     path('api/contactos/', ContactoListAPI.as_view(), name='api_contactos'),
