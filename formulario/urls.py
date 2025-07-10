aa4509e90170a1511584624a2dc536b75eb298d4
Jesdavtor <jestorniak@gmail.com>
2025-07-10 11:03:01 -0400

    Delete formulario directory

--- formulario/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ContactoCreateView, ContactoExitoView, ContactoListAPI
-
-urlpatterns = [
-    path('contacto/', ContactoCreateView.as_view(), name='contacto'),
-    path('contacto/exito/', ContactoExitoView.as_view(), name='contacto_exito'),
-    path('api/contactos/', ContactoListAPI.as_view(), name='api_contactos'),
-] 
